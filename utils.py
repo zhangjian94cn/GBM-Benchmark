b4c8cecd9490fa688cdb7dbdf6e148f3bfd560fe
zhangjian94cn <jian2.zhang@intel.com>
2022-09-14 08:42:59 +0000

    move classification_metrics

--- utils.py
+++ utils.py
@@ -3,11 +3,11 @@ import os
 import platform
 import subprocess
 import sys
+import numpy as np
+
+import sklearn.metrics as sklm
 from pathlib import Path
 from typing import Any, Dict, Iterable, List, Tuple, Union, cast
-
-
-
 
 
 
@@ -33,4 +33,24 @@ def generate_cases(params: Dict[str, Union[List[Any], Any]]) -> List[str]:
     return commands
 
 
+def classification_metrics(y_true, y_prob, threshold=0.5):
+
+    def evaluate_metrics(y_true, y_pred, metrics):
+        res = {}
+        for metric_name, metric in metrics.items():
+            res[metric_name] = metric(y_true, y_pred)
+        return res
+
+    y_true = y_true.to_numpy()
+
+    y_pred = np.where(y_prob > threshold, 1, 0)
+    metrics = {
+        "Accuracy": sklm.accuracy_score,
+        "Log_Loss": lambda real, pred: sklm.log_loss(real, y_prob, eps=1e-5),
+        # yes, I'm using y_prob here!
+        "AUC": lambda real, pred: sklm.roc_auc_score(real, y_prob),
+        "Precision": sklm.precision_score,
+        "Recall": sklm.recall_score,
+    }
+    return evaluate_metrics(y_true, y_pred, metrics)
 
